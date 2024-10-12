0ed1226b27463e606d9efe4941a1f478fc7b80ec
seo600 <conrad270@naver.com>
2024-10-12 16:53:34 +0900

    (Fix) 시간 기능 수정

--- Assets/Scripts/TimeUI.cs
+++ Assets/Scripts/TimeUI.cs
@@ -12,7 +12,5 @@ public class TimeUI : MonoBehaviour
     {
         DateTime time = DateTime.Now;
         timeText.text = time.ToString("HH:mm");
-        Debug.Log("시작");
-        Debug.Log("현재 시간은 : " + time.ToString("HH:mm"));
     }
 }
